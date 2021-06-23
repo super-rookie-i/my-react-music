b68c463578903801daf2efce1208b7994c6a068c
super-rookie-i <531117217@qq.com>
2021-06-23 20:26:36 +0800

    搭建基本骨架

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { IconStyle } from './assets/iconfont/iconfont';
 import { GlobalStyle } from './style';
+import { renderRoutes } from 'react-router-config'; //读取路由配置转化为Route标签
+import routes from './routes/index';
+import { HashRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store/index'
+
 
 function App () {
   return (
-    <div className="App">
-      <GlobalStyle></GlobalStyle>
-      <IconStyle></IconStyle>
-      <i className="iconfont">&#xe62b;</i>
-    </div>
+    <Provider store={store}>
+      <HashRouter>
+        <GlobalStyle></GlobalStyle>
+        <IconStyle></IconStyle>
+        { renderRoutes (routes) }
+      </HashRouter>
+    </Provider>
   );
 }
 
